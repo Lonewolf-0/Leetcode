b2ceab729e1aaed36edf9728724dc3fe32948e7a
Aryan Tyagi <aryan.tyagi202@gmail.com>
2025-11-16 19:12:16 +0530

    Time: 1 ms (57.25%) | Memory: 44 MB (16.13%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -7,10 +7,10 @@ class Solution {
     public void recursionSets(int[] nums, int idx, List<Integer> curr, List<List<Integer>> res){
         res.add(new ArrayList<>(curr));
 
-        for (int i = idx; i < nums.length; i++) {
-            curr.add(nums[i]);              
-            recursionSets(nums, i + 1, curr, res);  
-            curr.remove(curr.size() - 1);  
+        for(int i=idx; i<nums.length; i++){
+            curr.add(nums[i]);
+            recursionSets(nums, i+1, curr, res);
+            curr.remove(curr.size() - 1);
         }
 
     }
