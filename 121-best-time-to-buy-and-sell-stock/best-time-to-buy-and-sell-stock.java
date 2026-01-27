d9463f45a1db0bd1e3048ea584904b523e834fa9
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-01-27 21:31:55 +0530

    Time: 2 ms (41.84%) | Memory: 94.4 MB (62.15%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int min=prices[0];
+        for(int i=1; i<prices.length; i++){
+            ans = Math.max(prices[i]-min, ans);
+            min = Math.min(prices[i],min);
+        }
+        return ans;
+    }
+}
