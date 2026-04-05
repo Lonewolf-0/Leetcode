d6622cadd5239ffd0e550539ebadc79f98a0f7ae
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-04-05 19:22:49 +0530

    Time: 6 ms (99.79%) | Memory: 47.5 MB (94.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,26 +1,33 @@
 class Solution {
+    private long calculateTotalHours(int[] piles, int speed){
+    long totalH = 0;
+    for (int bananas: piles){
+        totalH += (bananas + speed - 1) / speed;
+    }
+    return totalH;
+}
     public int minEatingSpeed(int[] piles, int h) {
-        int start = 1;
-        int end = Integer.MAX_VALUE;
-        int res = Integer.MAX_VALUE;
+        int maxPile=0;
+        for(int i=0; i<piles.length; i++){
+            maxPile = Math.max(maxPile, piles[i]);
+        }
 
-        while(start<=end){
-            int pileSize = start + (end-start)/2;
-            int hours=0;
-            for (int pile : piles) {
-                hours += (int) Math.ceil((double) pile / pileSize);
-            }
-            
-            
-            if(hours<=h){
-                res = Math.min(res,pileSize);
-                end = pileSize-1;
+        int low = 1;
+        int high = maxPile;
+        int ans = maxPile;
+
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+long totalH = calculateTotalHours(piles, mid);
+            if(totalH<=h){
+                ans = mid;
+                high = mid-1;
             }
             else{
-                start = pileSize+1;
+                low = mid+1;
             }
         }
-        return res;
-    
+        return ans;
     }
 }
