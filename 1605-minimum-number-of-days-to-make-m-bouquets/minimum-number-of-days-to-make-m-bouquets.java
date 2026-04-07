d80cadcd1c57fbd6695b1d106537d1934bdcef8f
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-04-07 19:46:00 +0530

    Time: 7 ms (99.98%) | Memory: 82.5 MB (86.10%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,23 +1,51 @@
 class Solution {
-        public int minDays(int[] A, int m, int k) {
-        long n = A.length, left = 1, right = (int)1e9;
-        if ((long)m * k > n) return -1;
-        while (left < right) {
-            long mid = (left )+(right-left) / 2, flow = 0, bouq = 0;
-            for (int j = 0; j < n; ++j) {
-                if (A[j] > mid) {
-                    flow = 0;
-                } else if (++flow >= k) {
-                    bouq++;
-                    flow = 0;
-                }
-            }
-            if (bouq < m) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+
+        if ((long) m * k > n)
+            return -1;
+
+        int low = 1, high = 0;
+
+        for (int i : bloomDay) {
+            if (i > high)
+                high = i;
         }
-        return (int)left;
+
+        int ans = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isPossible(bloomDay, mid, m, k)) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+
+    public boolean isPossible(int[] bloomDay, int day, int m, int k) {
+        int count = 0;
+        int bouquets = 0;
+        int n = bloomDay.length;
+
+        for (int i = 0; i < n; i++) {
+            if (bloomDay[i] <= day) {
+                count++;
+                if (count == k) {
+                    bouquets++;
+                    count = 0;
+                }
+            } else {
+                count = 0;
+                
+                if ((m - bouquets) * k > n - i - 1)
+                    break;
+            }
+            if (bouquets >= m)
+                break;
+        }
+        return bouquets >= m;
     }
 }
