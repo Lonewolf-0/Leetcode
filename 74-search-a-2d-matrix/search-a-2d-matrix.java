be339fd0226ba3b7470edee0c67822e6698734ca
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-04-09 23:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (50.19%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix[0].length;
+        int m = matrix.length;
+        int r = 0;
+        int c = n-1;
+        while(r<m && c>-1){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            if(matrix[r][c]<target){
+                r++;
+            }
+            else{
+                c--;
+            }
+        }
+        return false;
+    }
+}
