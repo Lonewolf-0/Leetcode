27ba8458f36edbd3f28583b500bade7017030384
Aryan Tyagi <aryan.tyagi202@gmail.com>
2026-06-18 00:51:07 +0530

    Time: 19 ms (82.83%) | Memory: 19.3 MB (91.52%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,18 +1,27 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         n = len(nums)
+        freq = defaultdict(int)
         result = []
         
-        for i in range(n - k + 1):
-            window = nums[i:i+k]
+        for i in range(k):
+            freq[nums[i]] += 1
+        
+        def compute_xsum():
+            top = sorted(freq.items(), key=lambda p: (-p[1], -p[0]))[:x]
+            return sum(val * count for val, count in top)
+        
+        result.append(compute_xsum())
+        
+        for i in range(k, n):
+            out_elem = nums[i-k]
+            in_elem = nums[i]
             
-            freq = Counter(window)
+            freq[out_elem] -= 1
+            if freq[out_elem] == 0:
+                del freq[out_elem]
+            freq[in_elem] += 1
             
-            sorted_items = sorted(freq.items(), key=lambda p: (-p[1], -p[0]))
-            
-            top_x = sorted_items[:x]
-            
-            x_sum = sum(val * count for val, count in top_x)
-            result.append(x_sum)
+            result.append(compute_xsum())
         
         return result
